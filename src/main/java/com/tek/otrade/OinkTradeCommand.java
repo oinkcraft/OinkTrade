e069ea97c6024cb3975dc10a695b08064d7a5ae1
RedstoneTek <xxredstonetekxx@gmail.com>
2019-07-21 14:56:08 -0400

    Fixed help menu colors and added tab-completion.

--- src/main/java/com/tek/otrade/OinkTradeCommand.java
+++ src/main/java/com/tek/otrade/OinkTradeCommand.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -33,15 +35,14 @@ public class OinkTradeCommand implements CommandExecutor {
 		
 		if(args.length == 1) {
 			if(args[0].equalsIgnoreCase("help")) {
-				String helpMenu = "\n"
-								+ "&8/oinktrade help &9- &6Displays the help menu.\n"
-								+ "&8/oinktrade accept &9- &6Accepts the current trading request.\n"
-								+ "&8/oinktrade deny &9- &6Denies the current trading request.\n"
-								+ "&8/oinktrade send <player> &9- &6Sends a trading request to someone.\n";
+				String helpMenu = "&7/oinktrade help &9- &6Displays the help menu.\n"
+								+ "&7/oinktrade accept &9- &6Accepts the current trading request.\n"
+								+ "&7/oinktrade deny &9- &6Denies the current trading request.\n"
+								+ "&7/oinktrade send <player> &9- &6Sends a trading request to someone.\n";
 				
-				if(p.hasPermission(Reference.PERMISSION_SPY)) helpMenu += "&8/oinktrade spy <player> &9- &6Spies on a trading request.\n";
+				if(p.hasPermission(Reference.PERMISSION_SPY)) helpMenu += "&7/oinktrade spy <player> &9- &6Spies on a trading request.";
 				
-				p.sendMessage(helpMenu);
+				p.sendMessage(TextFormatter.color(helpMenu));
 			} 
 			
 			else if(args[0].equalsIgnoreCase("accept")) {
@@ -84,6 +85,22 @@ public class OinkTradeCommand implements CommandExecutor {
 		public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
 			if(sender instanceof Player) {
 				Player p = (Player) sender;
+				
+				if(args.length == 1) {
+					return Arrays.asList("help", "accept", "deny", "decline", "send", "spy").stream()
+							.filter(str -> str.toLowerCase().startsWith(args[0].toLowerCase()))
+							.collect(Collectors.toList());
+				}
+				
+				if(args.length == 2) {
+					if(args[0].equalsIgnoreCase("send") || args[0].equalsIgnoreCase("spy")) {
+						return Bukkit.getOnlinePlayers().stream()
+								.filter(player -> !player.equals(p))
+								.map(Player::getName)
+								.filter(name -> name.toLowerCase().startsWith(args[1].toLowerCase()))
+								.collect(Collectors.toList());
+					}
+				}
 			}
 			
 			return Arrays.asList();
