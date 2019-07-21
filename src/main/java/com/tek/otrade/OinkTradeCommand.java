7a9346c36c300902f80ed21dbe3a66654c5ad332
RedstoneTek <xxredstonetekxx@gmail.com>
2019-07-21 15:46:13 -0400

    Implemented the send subcommand, changed prefix colors.

--- src/main/java/com/tek/otrade/OinkTradeCommand.java
+++ src/main/java/com/tek/otrade/OinkTradeCommand.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -46,11 +48,31 @@ public class OinkTradeCommand implements CommandExecutor {
 			} 
 			
 			else if(args[0].equalsIgnoreCase("accept")) {
-				
+				if(p.hasPermission(Reference.PERMISSION_TRADE)) {
+					if(requests.containsKey(p.getUniqueId())) {
+						/* TODO ACCEPT REQUEST */
+					} else {
+						p.sendMessage(Reference.PREFIX + TextFormatter.color("&cYou have no trading requests on hold."));
+					}
+				} else {
+					p.sendMessage(Reference.PREFIX + Reference.NO_PERMISSIONS);
+				}
 			}
 			
 			else if(args[0].equalsIgnoreCase("deny") || args[0].equalsIgnoreCase("decline")) {
-				
+				if(p.hasPermission(Reference.PERMISSION_TRADE)) {
+					if(requests.containsKey(p.getUniqueId())) {
+						OfflinePlayer player = Main.getInstance().getServer().getOfflinePlayer(requests.get(p.getUniqueId()));
+						p.sendMessage(Reference.PREFIX + TextFormatter.color("&aDenied &6" + player.getName() + "&a's trading request."));
+						if(player.isOnline())
+							player.getPlayer().sendMessage(Reference.PREFIX + TextFormatter.color("&6" + p.getName() + "&c has denied your trading request."));
+						requests.remove(p.getUniqueId());
+					} else {
+						p.sendMessage(Reference.PREFIX + TextFormatter.color("&cYou have no trading requests on hold."));
+					}
+				} else {
+					p.sendMessage(Reference.PREFIX + Reference.NO_PERMISSIONS);
+				}
 			}
 			
 			else {
@@ -58,11 +80,54 @@ public class OinkTradeCommand implements CommandExecutor {
 			}
 		} else if(args.length == 2) {
 			if(args[0].equalsIgnoreCase("send")) {
-				
+				if(p.hasPermission(Reference.PERMISSION_TRADE)) {
+					String playerName = args[1];
+					Optional<Player> playerOpt = Bukkit.getOnlinePlayers().stream()
+							.filter(player -> player.getName().equalsIgnoreCase(playerName))
+							.map(player -> (Player)player)
+							.findFirst();
+					if(playerOpt.isPresent()) {
+						if(!playerOpt.get().equals(p)) {
+							if(playerOpt.get().getWorld().equals(p.getWorld())) {
+								if(!requests.containsKey(playerOpt.get().getUniqueId()) || !requests.get(playerOpt.get().getUniqueId()).equals(p.getUniqueId())) {
+									requests.put(playerOpt.get().getUniqueId(), p.getUniqueId());
+									p.sendMessage(Reference.PREFIX + TextFormatter.color("&aTrading request sent to &6" + playerOpt.get().getName() + "&a."));
+									playerOpt.get().sendMessage(Reference.PREFIX + TextFormatter.color("&6" + p.getName() + " &ahas sent you a trading request."));
+									playerOpt.get().sendMessage(Reference.PREFIX + TextFormatter.color("&9Use &6/oinktrade accept &9to begin trading."));
+									playerOpt.get().sendMessage(Reference.PREFIX + TextFormatter.color("&c&oNote that the request will expire in 30 seconds."));
+									
+									Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+										if(requests.containsKey(playerOpt.get().getUniqueId()) && requests.get(playerOpt.get().getUniqueId()).equals(p.getUniqueId())) {
+											requests.remove(playerOpt.get().getUniqueId());
+											if(p.isOnline())
+												p.sendMessage(Reference.PREFIX + TextFormatter.color("&cYour trading request to &6" + playerOpt.get().getName() + " &chas expired."));
+											if(playerOpt.get().isOnline())
+												playerOpt.get().sendMessage(Reference.PREFIX + TextFormatter.color("&cThe request from &6" + p.getName() + " &chas expired."));
+										}
+									}, 20 * 30);
+								} else {
+									p.sendMessage(Reference.PREFIX + TextFormatter.color("&cYou have already sent a request to this person."));
+								}
+							} else {
+								p.sendMessage(Reference.PREFIX + TextFormatter.color("&cYou must be in the same world to trade."));
+							}
+						} else {
+							p.sendMessage(Reference.PREFIX + TextFormatter.color("&cYou cannot trade with yourself."));
+						}
+					} else {
+						p.sendMessage(Reference.PREFIX + TextFormatter.color("&cNo player was found by that name."));
+					}
+				} else {
+					p.sendMessage(Reference.PREFIX + Reference.NO_PERMISSIONS);
+				}
 			}
 			
 			else if(args[0].equalsIgnoreCase("spy")) {
-				
+				if(p.hasPermission(Reference.PERMISSION_SPY)) {
+					
+				} else {
+					p.sendMessage(Reference.PREFIX + Reference.NO_PERMISSIONS);
+				}
 			}
 			
 			else {
