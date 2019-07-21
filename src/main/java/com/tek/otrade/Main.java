24804ff406ba7904181295cea13a8bc6c52bce6e
RedstoneTek <xxredstonetekxx@gmail.com>
2019-07-21 13:32:10 -0400

    Initial Commit (New)

---
+++ src/main/java/com/tek/otrade/Main.java
@@ -0,0 +1,51 @@
+package com.tek.otrade;
+
+import java.util.logging.Level;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.tek.rcore.RedstoneCore;
+
+import net.milkbowl.vault.economy.Economy;
+
+public class Main extends JavaPlugin {
+	
+	private static Main instance;
+	
+	private Economy economy;
+	private RedstoneCore redstoneCore;
+	
+	@Override
+	public void onEnable() {
+		instance = this;
+		
+		if(getServer().getPluginManager().getPlugin("Vault") != null) {
+			RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+			if(rsp != null) {
+				economy = rsp.getProvider();
+			} else {
+				getLogger().log(Level.SEVERE, "Couldn't find a valid economy, disabled plugin.");
+				getServer().getPluginManager().disablePlugin(this);
+				return;
+			}
+		} else {
+			getLogger().log(Level.SEVERE, "Vault is missing, disabled plugin.");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+		
+		if(getServer().getPluginManager().getPlugin("RedstoneCore") != null) {
+			redstoneCore = (RedstoneCore) getServer().getPluginManager().getPlugin("RedstoneCore");
+		} else {
+			getLogger().log(Level.SEVERE, "RedstoneCore is missing, disabled plugin.");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+	}
+	
+	public static Main getInstance() {
+		return instance;
+	}
+	
+}
